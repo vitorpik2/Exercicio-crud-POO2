28e451340c5c893973cfc90e3b53dc334526abb4
vitorpik2 <vitorfernandutragamerd@gmail.com>
2025-10-21 18:39:28 -0300

    segundo commit

--- restaurante/src/main/java/dao/ConexaoDAO.java
+++ restaurante/src/main/java/dao/ConexaoDAO.java
@@ -8,7 +8,7 @@ public class ConexaoDAO {
 
     private static final String URL = "jdbc:mysql://localhost:3306/restaurante_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "Root";
 
     public static Connection getConnection() {
         try {
